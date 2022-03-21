9c05e9e41c29f2e417e6ab784e93f1f57b28475c
Nikita Rydanov <wrongwayboyy@gmail.com>
2022-03-21 16:12:28 +0400

    Removed different folders for naive and arena versions

--- src/ast.c
+++ src/ast.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-#include <math.h>
-
+#include "arena/arena.h"
 #include "ast.h"
 
 unsigned int newnode (arena_t* arena, int nodetype, unsigned int l, unsigned int r) {
